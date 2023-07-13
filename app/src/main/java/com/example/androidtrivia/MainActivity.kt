ada1ef47eca5eae2fb77c22abe5d12b5bb03b168
jeanluckawel <jeanluckawel45@yahoo.com>
2023-07-12 21:48:43 -0800

    first commit

--- app/src/main/java/com/example/androidtrivia/MainActivity.kt
+++ app/src/main/java/com/example/androidtrivia/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.androidtrivia.ui.theme.AndroidTriviaTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,6 +39,21 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+@Composable
+fun HomePage(){
+}
+@Composable
+fun ButtonComponent(text:String, onclick:()->Unit){
+    Button(onClick = { /*TODO*/ },
+        ){
+        Text(
+            text = text,
+            fontWeight = FontWeight(200),
+            fontSize = 22.sp,
+            fontFamily = FontFamily.SansSerif
+        )
+    }
 }
 
 @Preview(showBackground = true)
